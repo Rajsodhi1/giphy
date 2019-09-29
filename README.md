2b6365825141e3c506bfd3473628fe544eb43c48
Raj Sodhi <rajsodhiesq@gmail.com>
2019-09-29 12:53:52 -0700

    giphy api working, appending to page

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Load preset buttons that generate gifs.
 Search form adds buttons to the page. 
 
 
+
 https://github.com/Rajsodhi1
 https://rajsodhi1.github.io/giphy/
--- giphyapp.js
+++ giphyapp.js
@@ -6,6 +6,7 @@ $(function() {
 var searchArray = ["Garnet", "Amethyst", "And Pearl", "And Steven!"];
 
 function populate(searchArray, classToAdd, areaToAddTo) {
+    //emoty the area otherwise we will end up adding copies or repeated buttons
     $(areaToAddTo).empty();
     for(var i=0; i<searchArray.length; i++) {
         var button =$("<button>");
@@ -16,3 +17,28 @@ function populate(searchArray, classToAdd, areaToAddTo) {
 
     }
 }
+
+$(document).on("click", ".search-input", function(){
+    var type = $(this).data("type");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +type+ "&apikey=MdlvCuWIdcqkMC7LAAZjaV5GvzPGPTHt&limit=10";
+    $.ajax({url:queryURL, method: "GET"})
+        .done(function(response){
+            for(var i=0; i<response.data.length; i++){
+                var searchDiv = $("<div class ='search-term'>");
+                var rating = response.data[i].rating;
+                var ratingText = $("<p>").text("Rating: "+rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $("<img>");
+                image.attr("src", still);
+                image.attr("data-still", still);
+                image.attr("data-animated", animated);
+                image.attr("data-state", "still");
+                image.addClass("searchImage");
+                searchDiv.append(ratingText);
+                searchDiv.append(image);
+                $("#searches").append(searchDiv);
+            }
+        })
+
+})
